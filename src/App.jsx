8875f3f423c6f0809a014ba42caf56466e68a344
Izqiaulhassan <izqiaulhassan166@gmail.com>
2026-02-28 22:07:51 +0500

    files added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Login from './Login'
+import Signup from './Signup'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-3xl'>starting a new project</div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>this is login form</div>
+  )
+}
+
+export default Login
